2e23cd74ca832db5baaed729aef84e163058e306
jonathanakhagbosu <33577242+jonathanakhagbosu@users.noreply.github.com>
2026-02-05 00:37:27 +0000

    Deploying to gh-pages from @ jonathanakhagbosu/jonathanakhagbosu.github.io@ff38d77dc423a8df076cce473f0a08028a39f0ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-résumé",
+          title: "résumé",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/assets/pdf/example_pdf.pdf";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course materials, schedules, and resources for classes taught.",
