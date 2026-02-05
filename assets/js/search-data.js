e3351f98c491bc1bc74b8b964dcadac4dcd193f7
jonathanakhagbosu <33577242+jonathanakhagbosu@users.noreply.github.com>
2026-02-05 03:30:11 +0000

    Deploying to gh-pages from @ jonathanakhagbosu/jonathanakhagbosu.github.io@726b3f71d480db0db2c6ac7a92919da72843d362 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-résumé",
-          title: "résumé",
+          title: "Résumé",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
