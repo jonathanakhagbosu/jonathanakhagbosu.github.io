0885bb5dcdc29bf5f7c8e5110c820deec58bf23c
jonathanakhagbosu <33577242+jonathanakhagbosu@users.noreply.github.com>
2026-02-18 06:38:30 +0000

    Deploying to gh-pages from @ jonathanakhagbosu/jonathanakhagbosu.github.io@a8809b1dc5e814dfc745af3cd411024777c4389c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/Jonathan%20Akhagbosu%20Resume.pdf";
+            window.location.href = "/assets/pdf/JA_Resume.pdf";
           },
         },{id: "post-a-post-with-plotly-js",
         
